df48da926cf8de284086c94bc50afa8ea3e53bb1
Ramon Almeida <ramonalmeida@icloud.com>
2021-03-11 09:50:19 -0300

    vue-teste crud componentes axios

---
+++ src/components/Tutorial.vue
@@ -0,0 +1,126 @@
+<template>
+  <div v-if="currentTutorial" class="edit-form">
+    <h4>adicionar txt</h4>
+    <form>
+      <div class="form-group">
+        <label for="title">titulo</label>
+        <input type="text" class="form-control" id="title"
+          v-model="currentTutorial.title"
+        />
+      </div>
+      <div class="form-group">
+        <label for="description">descricao</label>
+        <input type="text" class="form-control" id="description"
+          v-model="currentTutorial.description"
+        />
+      </div>
+
+      <div class="form-group">
+        <label><strong>Status:</strong></label>
+        {{ currentTutorial.published ? "Published" : "Pending" }}
+      </div>
+    </form>
+
+    <button class="badge badge-primary mr-2"
+      v-if="currentTutorial.published"
+      @click="updatePublished(false)"
+    >
+      nao publicado
+    </button>
+    <button v-else class="badge badge-primary mr-2"
+      @click="updatePublished(true)"
+    >
+      publicado
+    </button>
+
+    <button class="badge badge-danger mr-2"
+      @click="deleteTutorial"
+    >
+      Deletar
+    </button>
+
+    <button type="submit" class="badge badge-success"
+      @click="updateTutorial"
+    >
+      atualizar
+    </button>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import TutorialDataService from "../services/TutorialDataService";
+
+export default {
+  name: "tutorial",
+  data() {
+    return {
+      currentTutorial: null,
+      message: ''
+    };
+  },
+  methods: {
+    getTutorial(id) {
+      TutorialDataService.get(id)
+        .then(response => {
+          this.currentTutorial = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    updatePublished(status) {
+      var data = {
+        id: this.currentTutorial.id,
+        title: this.currentTutorial.title,
+        description: this.currentTutorial.description,
+        published: status
+      };
+
+      TutorialDataService.update(this.currentTutorial.id, data)
+        .then(response => {
+          this.currentTutorial.published = status;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    updateTutorial() {
+      TutorialDataService.update(this.currentTutorial.id, this.currentTutorial)
+        .then(response => {
+          console.log(response.data);
+          this.message = 'Atualizado com sucesso!';
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    deleteTutorial() {
+      TutorialDataService.delete(this.currentTutorial.id)
+        .then(response => {
+          console.log(response.data);
+          this.$router.push({ name: "tutorials" });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    this.message = '';
+    this.getTutorial(this.$route.params.id);
+  }
+};
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
